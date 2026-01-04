7c039066cde2cba8056298d86213e37b07d44346
Timofei Zhakov <tima@chemodax.net>
2026-01-04 23:22:25 +0000

    inline OPENSSL_cleanse()
    git-svn-id: https://svn.rinrab.com/rinrab/xhash/trunk@6052 0083e6b4-6090-234a-836e-88fcc467669c

--- include/crypto/md32_common.h
+++ include/crypto/md32_common.h
@@ -243,7 +243,7 @@ int HASH_FINAL(unsigned char *md, HASH_CTX *c)
     p -= HASH_CBLOCK;
     HASH_BLOCK_DATA_ORDER(c, p, 1);
     c->num = 0;
-    OPENSSL_cleanse(p, HASH_CBLOCK);
+    memset(p, 0, HASH_CBLOCK);
 
 # ifndef HASH_MAKE_STRING
 #  error "HASH_MAKE_STRING must be defined!"
