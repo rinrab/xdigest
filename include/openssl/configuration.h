21cc6ddac844bd35408e9f718272c0fcb3fb9ef7
Timofei Zhakov <tima@chemodax.net>
2026-01-04 22:16:11 +0000

    generate assembly files, includes, and source using the generator
    git-svn-id: https://svn.rinrab.com/rinrab/xhash/trunk@6040 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ include/openssl/configuration.h
@@ -0,0 +1,15 @@
+#ifndef OPENSSL_CONFIGURATION_H
+# define OPENSSL_CONFIGURATION_H
+# pragma once
+
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+# define OPENSSL_API_COMPAT 0x10000000L
+
+# ifdef  __cplusplus
+}
+# endif
+
+#endif
