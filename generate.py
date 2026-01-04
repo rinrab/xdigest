50926d9a57b1faa0f61ca5fd0d359071f997a996
Timofei Zhakov <tima@chemodax.net>
2026-01-04 22:15:20 +0000

    add generate.py script
    git-svn-id: file:///home/repos/rinrab/xhash/trunk@6039 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ generate.py
@@ -0,0 +1,101 @@
+import os
+import shutil
+import subprocess
+
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except:
+        pass
+
+crypto = "openssl/crypto"
+openssl_include = "openssl/include"
+
+asm = "asm"
+src = "src"
+
+def perlasm(config, file):
+    input = os.path.join(crypto, file + ".pl")
+    output = os.path.join(asm, config, file + ".S")
+
+    mkdir(os.path.dirname(output))
+
+    subprocess.call(["perl", input, config, output])
+
+os.environ["CC"] = "gcc"
+
+perlasm("elf", "sha/asm/sha1-586")
+perlasm("elf", "sha/asm/sha1-armv4-large")
+perlasm("elf", "sha/asm/sha1-armv8")
+perlasm("elf", "sha/asm/sha1-x86_64")
+perlasm("elf", "sha/asm/sha256-586")
+perlasm("elf", "sha/asm/sha256-armv4")
+perlasm("elf", "sha/asm/sha512-armv8")
+perlasm("elf", "sha/asm/sha512-x86_64")
+perlasm("elf", "sha/asm/sha512-586")
+perlasm("elf", "sha/asm/sha512-armv4")
+perlasm("elf", "sha/asm/sha512-armv8")
+perlasm("elf", "sha/asm/sha512-x86_64")
+
+sha = os.path.join(src, "sha")
+mkdir(sha)
+
+shutil.copy2(os.path.join(crypto, "sha/sha_local.h"), sha)
+shutil.copy2(os.path.join(crypto, "sha/sha1dgst.c"), sha)
+shutil.copy2(os.path.join(crypto, "sha/sha256.c"), sha)
+shutil.copy2(os.path.join(crypto, "sha/sha512.c"), sha)
+
+include = "include"
+mkdir(include)
+mkdir(os.path.join(include, "openssl"))
+mkdir(os.path.join(include, "internal"))
+mkdir(os.path.join(include, "crypto"))
+
+shutil.copy2(os.path.join(openssl_include, "openssl/sha.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/macros.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/e_os2.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/opensslconf.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/types.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/symhacks.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/cryptoerr.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/cryptoerr_legacy.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/core.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/stack.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(openssl_include, "openssl/safestack.h"),
+             os.path.join(include, "openssl"))
+
+shutil.copy2(os.path.join(openssl_include, "internal/endian.h"),
+             os.path.join(include, "internal"))
+shutil.copy2(os.path.join(openssl_include, "internal/deprecated.h"),
+             os.path.join(include, "internal"))
+shutil.copy2(os.path.join(openssl_include, "internal/common.h"),
+             os.path.join(include, "internal"))
+shutil.copy2(os.path.join(openssl_include, "internal/cryptlib.h"),
+             os.path.join(include, "internal"))
+shutil.copy2(os.path.join(openssl_include, "internal/e_os.h"),
+             os.path.join(include, "internal"))
+shutil.copy2(os.path.join(openssl_include, "internal/numbers.h"),
+             os.path.join(include, "internal"))
+shutil.copy2(os.path.join(openssl_include, "internal/nelem.h"),
+             os.path.join(include, "internal"))
+
+shutil.copy2(os.path.join(openssl_include, "crypto/md32_common.h"),
+             os.path.join(include, "crypto"))
+shutil.copy2(os.path.join(openssl_include, "crypto/sha.h"),
+             os.path.join(include, "crypto"))
+
+shutil.copy2(os.path.join(include, "custom/configuration.h"),
+             os.path.join(include, "openssl"))
+shutil.copy2(os.path.join(include, "custom/opensslv.h"),
+             os.path.join(include, "openssl"))
+
