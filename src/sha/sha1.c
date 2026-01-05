38f1136c3181877c6664b29b4b08415745ab7539
Timofei Zhakov <tima@chemodax.net>
2026-01-05 22:07:03 +0000

    implement contextless computation of SHA1 digest
    git-svn-id: file:///home/repos/rinrab/xhash/trunk@6088 0083e6b4-6090-234a-836e-88fcc467669c

--- src/sha/sha1.c
+++ src/sha/sha1.c
@@ -1,2 +1,18 @@
+#include <xhash/sha.h>
+
 #include "sha_local.h"
 
+unsigned char *xhash_sha1(const unsigned char *d, size_t n, unsigned char *md)
+{
+    static unsigned char m[SHA_DIGEST_LENGTH];
+    xhash_sha1_ctx_t ctx;
+
+    if (md == NULL)
+        md = m;
+
+    xhash_sha1_init(&ctx);
+    xhash_sha1_update(&ctx, d, n);
+    xhash_sha1_final(md, &ctx);
+
+    return md;
+}
