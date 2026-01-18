7a3d5c305b4bea55844d709cbac30884be7dccaa
Timofei Zhakov <tima@chemodax.net>
2026-01-18 19:50:28 +0000

    Export public initialization function (xhash_init).
    git-svn-id: file:///home/repos/rinrab/xhash/trunk@6113 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ include/xhash/core.h
@@ -0,0 +1,11 @@
+#ifndef xhash_CORE_H
+# define xhash_CORE_H
+# pragma once
+
+/*
+ * Initializes the library. This function must be called once per process.
+ */
+void xhash_init(void);
+
+#endif
+
