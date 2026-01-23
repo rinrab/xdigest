ed3567cc7a4a862ac3b4c688c67a2ae25a785318
Timofei Zhakov <tima@chemodax.net>
2026-01-23 22:10:19 +0000

    Don't use that 'p_' prefix when the return value is intended to be output to a pointer.
    git-svn-id: https://svn.rinrab.com/rinrab/xhash/trunk@6248 0083e6b4-6090-234a-836e-88fcc467669c

--- include/xhash/md2.h
+++ include/xhash/md2.h
@@ -17,18 +17,18 @@ typedef struct xhash_md2_ctx_t xhash_md5_ctx_t;
 
 #define XHASH_MD2_DIGEST_LENGTH 16
 
-int xhash_md2_init(xhash_md5_ctx_t *p_ctx);
+int xhash_md2_init(xhash_md5_ctx_t *ctx);
 
 int xhash_md2_update(xhash_md5_ctx_t *ctx,
                      const void *data,
                      size_t len);
 
-int xhash_md2_final(unsigned char *p_digest,
+int xhash_md2_final(unsigned char *digest,
                     xhash_md2_ctx_t *ctx);
 
 void xhash_md2(const unsigned char *data,
                size_t len,
-               unsigned char *p_digest);
+               unsigned char *digest);
 
 #ifdef __cplusplus
 }
