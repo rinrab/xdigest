4505bf797c217ecb21c989c83dd93a48fbaee203
Timofei Zhakov <tima@chemodax.net>
2026-01-23 17:22:30 +0000

    Remvoe 'inline_dummy_export.patch' that was affecting ebdic support that was droped few changes ago.
    git-svn-id: https://svn.rinrab.com/rinrab/xhash/trunk@6244 0083e6b4-6090-234a-836e-88fcc467669c

--- generate.py
+++ generate.py
@@ -232,7 +232,6 @@ include("crypto/md32_common.h")
 include("crypto/sha.h")
 
 patch("patches/xhash_cleanse.patch")
-patch("patches/inline_dummy_export.patch")
 patch("patches/OPENSSL_IA32CAP_P_MAX_INDEXES.patch")
 patch("patches/sha256_hash_data.patch")
 patch("patches/sha512_hash_data.patch")
