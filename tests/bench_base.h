f1334900af91d8a33ad6ec091c53acf93c530f6d
Timofei Zhakov <tima@chemodax.net>
2026-01-24 09:10:11 +0000

    Factor-out benchmarking framework into separate headers.
    git-svn-id: file:///home/repos/rinrab/xhash/trunk@6249 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ tests/bench_base.h
@@ -0,0 +1,84 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "time.h"
+
+static void
+xhash_bench__create_buffer(unsigned char *buf, size_t len)
+{
+    int i;
+    uint32_t seed = 67;
+
+    for (i = 0; i < len; i++)
+    {
+        seed = (seed * 1103515245UL + 12345UL) & 0xffffffffUL;
+        buf[i] = seed & 0xff;
+    }
+}
+
+typedef struct xhash_bench__ctx_t {
+    const char *name;
+
+#ifdef WIN32
+    #error "not supported"
+#else
+    xhash_time__t start;
+    xhash_time__t end;
+#endif
+
+    double requested_time;
+    size_t iter;
+
+    size_t bufsize;
+    unsigned char *buf;
+} xhash_bench__ctx_t;
+
+static xhash_bench__ctx_t *
+xhash_bench__ctx_create(const char *name, double requested_time, size_t bufsize)
+{
+    xhash_bench__ctx_t *ctx = (xhash_bench__ctx_t *)malloc(sizeof(*ctx));
+
+    ctx->name = name;
+    ctx->requested_time = requested_time;
+    ctx->start = xhash_time__get();
+    ctx->iter = 0;
+
+    ctx->bufsize = bufsize;
+    ctx->buf = (unsigned char *)malloc(bufsize);
+    xhash_bench__create_buffer(ctx->buf, bufsize);
+
+    return ctx;
+}
+
+static int
+xhash_bench__ctx_next(xhash_bench__ctx_t *ctx)
+{
+    double diff;
+
+    ctx->iter++;
+    ctx->end = xhash_time__get();
+
+    diff = xhash_time__diff(ctx->start, ctx->end);
+
+    return (diff < ctx->requested_time);
+}
+
+static void
+xhash_bench__ctx_finish(xhash_bench__ctx_t *ctx)
+{
+#define GB (1024 * 1024 * 1024)
+
+    printf("%6s: Processed %.2f GB in %.2f seconds.\n",
+           ctx->name,
+           ((double)ctx->bufsize * ctx->iter) / GB,
+           xhash_time__diff(ctx->start, ctx->end));
+}
+
+static void
+xhash_bench__ctx_free(xhash_bench__ctx_t *ctx)
+{
+    free(ctx->buf);
+    free(ctx);
+}
+
--- tests/test_xhash.c
+++ tests/test_xhash.c
@@ -6,79 +6,32 @@
 #include <xhash/sha1.h>
 #include <xhash/xhash.h>
 
-#define BUFSIZE 1024 * 1024
-
-#if WIN32
-# error "TODO"
-#else
-#include <time.h>
-
-#define BILLION  1000000000L;
-
-typedef struct timespec time__t;
-
-time__t time__get()
-{
-    struct timespec time;
-
-    int status = clock_gettime(CLOCK_REALTIME, &time);
-
-    if (status == -1)
-    {
-        perror("clock_gettime");
-        abort();
-    }
-
-    return time;
-}
-
-double time__diff(time__t start, time__t end)
-{
-    return (end.tv_sec - start.tv_sec)
-             + (double)(end.tv_nsec - start.tv_nsec)
-               / (double)BILLION;
-}
-#endif
+#include "bench_base.h"
 
 int setup_tests(void);
 
 int main()
 {
     xhash_sha1_ctx_t *ctx = malloc(XHASH_SHA1_CTX_SIZE);
-    unsigned char digest[XHASH_SHA1_DIGEST_LENGTH];
-    unsigned char *buf;
+    xhash_bench__ctx_t *bench;
 
-    time__t start, end;
-    size_t iter = 0, i;
-    uint32_t seed = 67;
+    unsigned char digest[XHASH_SHA1_DIGEST_LENGTH];
 
     xhash_init();
 
-    buf = malloc(BUFSIZE);
-    for (i = 0; i < BUFSIZE; i++)
-    {
-        seed = (seed * 1103515245UL + 12345UL) & 0xffffffffUL;
-        buf[i] = seed & 0xff;
-    }
-
-    start = time__get();
     xhash_sha1_init(ctx);
 
-    do
+    bench = xhash_bench__ctx_create("SHA1", 1.0, 1024 * 1024);
+
+    while (xhash_bench__ctx_next(bench))
     {
-        xhash_sha1_update(ctx, buf, BUFSIZE);
-        end = time__get();
-        iter++;
+        xhash_sha1_update(ctx, bench->buf, bench->bufsize);
     }
-    while (time__diff(start, end) < 1.0);
 
     xhash_sha1_final(digest, ctx);
 
-    printf("Processed %.2f GB in %.2f seconds.\n",
-           ((double)BUFSIZE * iter) / (1024 * 1024 * 1024),
-           time__diff(start, end));
-
-    free(buf);
+    xhash_bench__ctx_finish(bench);
+    xhash_bench__ctx_free(bench);
 
     setup_tests();
 
