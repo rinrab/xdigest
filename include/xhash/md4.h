750a8265a0df68afd72fd966e99f2173dccc0bba
Timofei Zhakov <tima@chemodax.net>
2026-01-24 21:17:09 +0000

    Fixup typedefs for checksum contexts in include files.
    git-svn-id: file:///home/repos/rinrab/xhash/trunk@6250 0083e6b4-6090-234a-836e-88fcc467669c

--- include/xhash/md4.h
+++ include/xhash/md4.h
@@ -13,13 +13,13 @@ extern "C" {
  * MD4
  */
 #define XHASH_MD4_CTX_SIZE 92
-typedef struct xhash_md4_ctx_t xhash_md5_ctx_t;
+typedef struct xhash_md4_ctx_t xhash_md4_ctx_t;
 
 #define XHASH_MD4_DIGEST_LENGTH 16
 
-int xhash_md4_init(xhash_md5_ctx_t *ctx);
+int xhash_md4_init(xhash_md4_ctx_t *ctx);
 
-int xhash_md4_update(xhash_md5_ctx_t *ctx,
+int xhash_md4_update(xhash_md4_ctx_t *ctx,
                      const void *data,
                      size_t len);
 
