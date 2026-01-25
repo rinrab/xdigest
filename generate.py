fe7510334e3ee2f711be8b9afb1ec982518d3afd
Timofei Zhakov <tima@chemodax.net>
2026-01-25 12:36:12 +0000

    Cleanup hash data functions for MD family.
    git-svn-id: file:///home/repos/rinrab/xdigest/trunk@6269 0083e6b4-6090-234a-836e-88fcc467669c

--- generate.py
+++ generate.py
@@ -235,8 +235,12 @@ patch("patches/xdig_cleanse.patch")
 patch("patches/OPENSSL_IA32CAP_P_MAX_INDEXES.patch")
 patch("patches/sha256_hash_data.patch")
 patch("patches/sha512_hash_data.patch")
-patch("patches/ctx_size_funcs.patch")
+patch("patches/md2_hash_data.patch")
+patch("patches/md4_hash_data.patch")
+patch("patches/md5_hash_data.patch")
 patch("patches/die_and_assert.patch")
+
+patch("patches/ctx_size_funcs.patch")
 
 patch("patches/mem_clr_remove_callback.patch")
 patch("patches/mem_clr_ifdef.patch")
