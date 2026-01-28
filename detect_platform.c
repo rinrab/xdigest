304172024abea191b1ca15843cadcbbeae08fbbd
Timofei Zhakov <tima@chemodax.net>
2026-01-28 11:00:44 +0000

    Add smart platform detection using an external file compilation and reding its error, which we are using to dumb detected architecture.
    git-svn-id: file:///home/repos/rinrab/xdigest/trunk@6332 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ detect_platform.c
@@ -0,0 +1,33 @@
+/*
+ * This is one of the most beautyfull ways to detect architecture. We compile
+ * this snippet in our cmake script. and scrape its error output, matching the
+ * correct configuration via dedicated keyword..
+ *
+ * Native cmake tools sometimes behaves upredictebly and are anoying to deal
+ * with. This is IMO the most relayable way to tackle this issue. This way we
+ * integrate directly to the compile. Because sometimes not the options define
+ * architecture, but its built into the program itself. Especially on Unix-ish
+ * platforms.
+ *
+ * We might also need a separte detector which checks whether the compiler
+ * works in general. Because this script takes error output entirely. And
+ * always fails no matter what.
+ *
+ * We cannot simply run it because then we would lose ability to cross-compile.
+ * It's not usually possible to run program compiled for different
+ * architecture on host one.
+ */
+
+#ifdef __linux__
+
+#if defined(__x86_64__) || defined(_M_X64)
+# error "DETECTED: linux64"
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+# error "DETECTED: linux32"
+#elif defined(__aarch64__) || defined(_M_ARM64)
+# error "DETECTED: linuxaarch64"
+#endif
+
+#else
+# error "DETECTED: unsupported"
+#endif
