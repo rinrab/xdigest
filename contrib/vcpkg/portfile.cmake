551b5375ee7b5b1660d77d2ff30fec90a4254386
Timofei Zhakov <tima@chemodax.net>
2026-01-29 13:21:06 +0000

    Compile using cmake for vcpkg port.
    git-svn-id: https://svn.rinrab.com/rinrab/xdigest/trunk@6364 0083e6b4-6090-234a-836e-88fcc467669c

--- contrib/vcpkg/portfile.cmake
+++ contrib/vcpkg/portfile.cmake
@@ -12,43 +12,15 @@ vcpkg_check_features(
         asm USE_ASM
 )
 
-set(OPTION "")
-
-if (NOT USE_ASM)
-    list(APPEND OPTIONS NO_ASM=1)
-endif()
-
-if(VCPKG_TARGET_IS_LINUX)
-    if(VCPKG_TARGET_ARCHITECTURE MATCHES "arm64")
-        set(ENV{ARCH} aarch64)
-    elseif(VCPKG_TARGET_ARCHITECTURE MATCHES "x64")
-        set(ENV{ARCH} x86_64)
-    elseif(VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
-        set(ENV{ARCH} x86)
-    else()
-        message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-    endif()
-endif()
-
-if (VCPKG_TARGET_IS_WINDOWS)
-    # todo
-else()
-    file(COPY "${CMAKE_CURRENT_LIST_DIR}/configure" DESTINATION "${SOURCE_PATH}")
-
-    vcpkg_configure_make(
-        SOURCE_PATH "${SOURCE_PATH}"
-        COPY_SOURCE
-        OPTIONS_RELEASE "--vcpkg-prefix=${CURRENT_PACKAGES_DIR}"
-        OPTIONS_DEBUG "--vcpkg-prefix=${CURRENT_PACKAGES_DIR}/debug"
-    )
-
-    vcpkg_build_make(
-        BUILD_TARGET install
-        MAKEFILE Makefile.conf
-    )
-endif()
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+)
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
