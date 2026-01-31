8c7dd1e64dac2857ac1ebd6cb08f872fc3cc221e
Timofei Zhakov <tima@chemodax.net>
2026-01-31 00:50:12 +0000

    vcpkg: fixup cmake config
    git-svn-id: https://svn.rinrab.com/rinrab/xdigest/trunk@6430 0083e6b4-6090-234a-836e-88fcc467669c

--- contrib/vcpkg/portfile.cmake
+++ contrib/vcpkg/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/xdigest")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
