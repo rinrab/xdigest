244e435f2916ec202eca921f34a76a8acf5c9c1f
Timofei Zhakov <tima@chemodax.net>
2026-02-01 12:08:21 +0000

    Don't forget to change type of data buffer our whole-block API consumes from unisgned-char-pointer to void-pointer.
    git-svn-id: file:///home/repos/rinrab/xdigest/trunk@6476 0083e6b4-6090-234a-836e-88fcc467669c

--- xdigest/md4/md4_one.c
+++ xdigest/md4/md4_one.c
@@ -24,7 +24,7 @@
 #endif
 
 unsigned char *
-xdig_md4(const unsigned char *data, size_t len, unsigned char *digest)
+xdig_md4(const void *data, size_t len, unsigned char *digest)
 {
     xdig_md4_ctx_t ctx;
 
