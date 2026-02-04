3675aeabcb1f9214632af0b6b5c1ff146480e0ab
Timofei Zhakov <tima@chemodax.net>
2026-02-04 21:06:03 +0000

    Strip away assertion functionality from common.h header since it has zero usages throughout the codebase.
    git-svn-id: file:///home/repos/rinrab/xdigest/trunk@6505 0083e6b4-6090-234a-836e-88fcc467669c

--- xdigest/include/internal/common.h
+++ xdigest/include/internal/common.h
@@ -49,18 +49,6 @@ xdig_die(const char *message, const char *file, int line)
     abort();
 }
 
-__owur static ossl_inline int ossl_assert_int(int expr, const char *exprstr,
-                                              const char *file, int line)
-{
-    if (!expr)
-        xdig_die(exprstr, file, line);
-
-    return expr;
-}
-
-#  define ossl_assert(x) ossl_assert_int((x) != 0, "Assertion failed: "#x, \
-                                         __FILE__, __LINE__)
-
 # endif
 
 void xdig_cleanse(void *ptr, size_t len);
