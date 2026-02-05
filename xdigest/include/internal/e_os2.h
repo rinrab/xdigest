a5249aba6a26906b3dee4e74407183a2a0a8917f
Timofei Zhakov <tima@chemodax.net>
2026-02-05 21:27:33 +0000

    Cleanup e_os2.h header from some IO stuff and other things we should not use.
    git-svn-id: https://svn.rinrab.com/rinrab/xdigest/trunk@6522 0083e6b4-6090-234a-836e-88fcc467669c

--- xdigest/include/internal/e_os2.h
+++ xdigest/include/internal/e_os2.h
@@ -11,13 +11,6 @@
 # define xdig_E_OS2_H
 # pragma once
 
-/* ignored include 'openssl/macros.h' */
-# ifndef xdig_NO_DEPRECATED_3_0
-#  define HEADER_E_OS2_H
-# endif
-
-/* ignored include 'openssl/opensslconf.h' */
-
 #ifdef  __cplusplus
 extern "C" {
 #endif
@@ -157,66 +150,6 @@ extern "C" {
  * That's it for OS-specific stuff
  *****************************************************************************/
 
-/*-
- * xdig_EXTERN is normally used to declare a symbol with possible extra
- * attributes to handle its presence in a shared library.
- * xdig_EXPORT is used to define a symbol with extra possible attributes
- * to make it visible in a shared library.
- * Care needs to be taken when a header file is used both to declare and
- * define symbols.  Basically, for any library that exports some global
- * variables, the following code must be present in the header file that
- * declares them, before xdig_EXTERN is used:
- *
- * #ifdef SOME_BUILD_FLAG_MACRO
- * # undef xdig_EXTERN
- * # define xdig_EXTERN xdig_EXPORT
- * #endif
- *
- * The default is to have xdig_EXPORT and xdig_EXTERN
- * have some generally sensible values.
- */
-
-# if defined(xdig_SYS_WINDOWS) && defined(xdig_OPT_WINDLL)
-#  define xdig_EXPORT extern __declspec(dllexport)
-#  define xdig_EXTERN extern __declspec(dllimport)
-# else
-#  define xdig_EXPORT extern
-#  define xdig_EXTERN extern
-# endif
-
-# ifdef _WIN32
-#  ifdef _WIN64
-#   define ossl_ssize_t __int64
-#   define OSSL_SSIZE_MAX _I64_MAX
-#  else
-#   define ossl_ssize_t int
-#   define OSSL_SSIZE_MAX INT_MAX
-#  endif
-# endif
-
-# if defined(xdig_SYS_UEFI) && !defined(ossl_ssize_t)
-#  define ossl_ssize_t INTN
-#  define OSSL_SSIZE_MAX MAX_INTN
-# endif
-
-# ifndef ossl_ssize_t
-#  include <sys/types.h>
-#  define ossl_ssize_t ssize_t
-#  if defined(SSIZE_MAX)
-#   define OSSL_SSIZE_MAX SSIZE_MAX
-#  elif defined(_POSIX_SSIZE_MAX)
-#   define OSSL_SSIZE_MAX _POSIX_SSIZE_MAX
-#  else
-#   define OSSL_SSIZE_MAX ((ssize_t)(SIZE_MAX>>1))
-#  endif
-# endif
-
-# if defined(UNUSEDRESULT_DEBUG)
-#  define __owur __attribute__((__warn_unused_result__))
-# else
-#  define __owur
-# endif
-
 /* Standard integer types */
 # define xdig_NO_INTTYPES_H
 # define xdig_NO_STDINT_H
@@ -256,52 +189,6 @@ typedef unsigned __int64 uint64_t;
 # else
 #  include <stdint.h>
 #  undef xdig_NO_STDINT_H
-# endif
-# if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L && \
-    defined(INTMAX_MAX) && defined(UINTMAX_MAX)
-typedef intmax_t ossl_intmax_t;
-typedef uintmax_t ossl_uintmax_t;
-# else
-/* Fall back to the largest we know we require and can handle */
-typedef int64_t ossl_intmax_t;
-typedef uint64_t ossl_uintmax_t;
-# endif
-
-/* ossl_inline: portable inline definition usable in public headers */
-# if !defined(inline) && !defined(__cplusplus)
-#  if defined(__STDC_VERSION__) && __STDC_VERSION__>=199901L
-   /* just use inline */
-#   define ossl_inline inline
-#  elif defined(__GNUC__) && __GNUC__>=2
-#   define ossl_inline __inline__
-#  elif defined(_MSC_VER)
-  /*
-   * Visual Studio: inline is available in C++ only, however
-   * __inline is available for C, see
-   * http://msdn.microsoft.com/en-us/library/z8y1yy88.aspx
-   */
-#   define ossl_inline __inline
-#  else
-#   define ossl_inline
-#  endif
-# else
-#  define ossl_inline inline
-# endif
-
-# if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L && \
-     !defined(__cplusplus)
-#  define ossl_noreturn _Noreturn
-# elif defined(__GNUC__) && __GNUC__ >= 2
-#  define ossl_noreturn __attribute__((noreturn))
-# else
-#  define ossl_noreturn
-# endif
-
-/* ossl_unused: portable unused attribute for use in public headers */
-# if defined(__GNUC__)
-#  define ossl_unused __attribute__((unused))
-# else
-#  define ossl_unused
 # endif
 
 #ifdef  __cplusplus
