954dd04f5899f6f6d452c10209f92d28b8173728
Timofei Zhakov <tima@chemodax.net>
2026-02-20 08:55:15 +0000

    Implement a bunch of algorithms into rust high level API.
    git-svn-id: https://svn.rinrab.com/rinrab/xdigest/trunk@6671 0083e6b4-6090-234a-836e-88fcc467669c

--- contrib/rust/xdigest/src/lib.rs
+++ contrib/rust/xdigest/src/lib.rs
@@ -73,7 +73,23 @@ macro_rules! define_algorithm {
     };
 }
 
+/* SHA-1 */
+pub const SHA1_DIGEST_LENGTH: usize = XDIG_SHA1_DIGEST_LENGTH as usize;
+
+define_algorithm!(
+    SHA1,
+    xdig_sha1_ctx_t,
+    xdig_sha1_ctx_size,
+    xdig_sha1_ctx_init,
+    xdig_sha1_ctx_update,
+    xdig_sha1_ctx_final,
+    xdig_sha1,
+    SHA1_DIGEST_LENGTH
+);
+
+/* SHA-2 (256) */
 pub const SHA256_DIGEST_LENGTH: usize = XDIG_SHA256_DIGEST_LENGTH as usize;
+pub const SHA224_DIGEST_LENGTH: usize = XDIG_SHA224_DIGEST_LENGTH as usize;
 
 define_algorithm!(
     SHA256,
@@ -86,3 +102,80 @@ define_algorithm!(
     SHA256_DIGEST_LENGTH
 );
 
+define_algorithm!(
+    SHA224,
+    xdig_sha224_ctx_t,
+    xdig_sha224_ctx_size,
+    xdig_sha224_ctx_init,
+    xdig_sha224_ctx_update,
+    xdig_sha224_ctx_final,
+    xdig_sha224,
+    SHA224_DIGEST_LENGTH
+);
+
+/* SHA-2 (512) */
+pub const SHA512_DIGEST_LENGTH: usize = XDIG_SHA512_DIGEST_LENGTH as usize;
+pub const SHA384_DIGEST_LENGTH: usize = XDIG_SHA384_DIGEST_LENGTH as usize;
+
+define_algorithm!(
+    SHA512,
+    xdig_sha512_ctx_t,
+    xdig_sha512_ctx_size,
+    xdig_sha512_ctx_init,
+    xdig_sha512_ctx_update,
+    xdig_sha512_ctx_final,
+    xdig_sha512,
+    SHA512_DIGEST_LENGTH
+);
+
+
+define_algorithm!(
+    SHA384,
+    xdig_sha384_ctx_t,
+    xdig_sha384_ctx_size,
+    xdig_sha384_ctx_init,
+    xdig_sha384_ctx_update,
+    xdig_sha384_ctx_final,
+    xdig_sha384,
+    SHA384_DIGEST_LENGTH
+);
+
+/* MD5 */
+pub const MD5_DIGEST_LENGTH: usize = XDIG_MD5_DIGEST_LENGTH as usize;
+
+define_algorithm!(
+    MD5,
+    xdig_md5_ctx_t,
+    xdig_md5_ctx_size,
+    xdig_md5_ctx_init,
+    xdig_md5_ctx_update,
+    xdig_md5_ctx_final,
+    xdig_md5,
+    MD5_DIGEST_LENGTH
+);
+
+pub const MD4_DIGEST_LENGTH: usize = XDIG_MD4_DIGEST_LENGTH as usize;
+
+define_algorithm!(
+    MD4,
+    xdig_md4_ctx_t,
+    xdig_md4_ctx_size,
+    xdig_md4_ctx_init,
+    xdig_md4_ctx_update,
+    xdig_md4_ctx_final,
+    xdig_md4,
+    MD4_DIGEST_LENGTH
+);
+
+pub const MD2_DIGEST_LENGTH: usize = XDIG_MD2_DIGEST_LENGTH as usize;
+
+define_algorithm!(
+    MD2,
+    xdig_md2_ctx_t,
+    xdig_md2_ctx_size,
+    xdig_md2_ctx_init,
+    xdig_md2_ctx_update,
+    xdig_md2_ctx_final,
+    xdig_md2,
+    MD2_DIGEST_LENGTH
+);
