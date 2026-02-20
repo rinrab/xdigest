d4da2380604ee21d5bbad92ed7fc7dd56998e40f
Timofei Zhakov <tima@chemodax.net>
2026-02-20 08:47:32 +0000

    Use macro rule to define algorithm wrapper.
    git-svn-id: https://svn.rinrab.com/rinrab/xdigest/trunk@6670 0083e6b4-6090-234a-836e-88fcc467669c

--- contrib/rust/xdigest/src/lib.rs
+++ contrib/rust/xdigest/src/lib.rs
@@ -12,55 +12,77 @@ pub trait XDigest<const DIGEST_SIZE: usize> {
     fn digest(data: &[u8]) -> [u8; DIGEST_SIZE];
 }
 
-pub struct SHA256 {
-    ptr: *mut xdig_sha256_ctx_t,
-    _storage: Box<[MaybeUninit<u8>]>,
+macro_rules! define_algorithm {
+    (
+        $struct:ident,
+        $ctx_c:ident,
+        $ctx_size_c:ident,
+        $ctx_init_c:ident,
+        $ctx_update_c:ident,
+        $ctx_final_c:ident,
+        $digest_c:ident,
+        $digest_size:expr
+    ) => {
+        pub struct $struct {
+            ptr: *mut $ctx_c,
+            _storage: Box<[MaybeUninit<u8>]>,
+        }
+
+        impl XDigest<$digest_size> for $struct {
+            fn new() -> Self {
+                let ctx_size = unsafe { $ctx_size_c() };
+                let storage = vec![MaybeUninit::uninit(); ctx_size].into_boxed_slice();
+                let ptr = storage.as_ptr() as *mut $ctx_c;
+                unsafe { $ctx_init_c(ptr) };
+                return Self { ptr, _storage: storage }
+            }
+
+            fn update(&mut self, data: &[u8]) {
+                unsafe {
+                    $ctx_update_c(
+                        self.ptr,
+                        data.as_ptr() as *const std::os::raw::c_void,
+                        data.len()
+                    );
+                };
+            }
+
+            fn finalise(&mut self) -> [u8; $digest_size] {
+                let mut result = [0u8; $digest_size];
+                unsafe {
+                    $ctx_final_c(
+                        self.ptr,
+                        result.as_mut_ptr() as *mut std::os::raw::c_uchar,
+                    );
+                };
+                return result;
+            }
+
+            fn digest(data: &[u8]) -> [u8; $digest_size] {
+                let mut result = [0u8; $digest_size];
+                unsafe {
+                    $digest_c(
+                        result.as_mut_ptr() as *mut std::os::raw::c_uchar,
+                        data.as_ptr() as *const std::os::raw::c_void,
+                        data.len(),
+                    );
+                };
+                return result;
+            }
+        }
+    };
 }
 
-const SHA256_SIZE: usize = XDIG_SHA256_DIGEST_LENGTH as usize;
-impl XDigest<SHA256_SIZE> for SHA256 {
-    fn new() -> Self {
-        let ctx_size = unsafe {
-            xdig_sha256_ctx_size()
-        };
-        let storage = vec![MaybeUninit::uninit(); ctx_size].into_boxed_slice();
-        let ptr = storage.as_ptr() as *mut xdig_sha256_ctx_t;
-        unsafe {
-            xdig_sha256_ctx_init(ptr);
-        };
-        return Self { ptr, _storage: storage }
-    }
+pub const SHA256_DIGEST_LENGTH: usize = XDIG_SHA256_DIGEST_LENGTH as usize;
 
-    fn update(&mut self, data: &[u8]) {
-        unsafe {
-            xdig_sha256_ctx_update(
-                self.ptr,
-                data.as_ptr() as *const std::os::raw::c_void,
-                data.len()
-            );
-        };
-    }
+define_algorithm!(
+    SHA256,
+    xdig_sha256_ctx_t,
+    xdig_sha256_ctx_size,
+    xdig_sha256_ctx_init,
+    xdig_sha256_ctx_update,
+    xdig_sha256_ctx_final,
+    xdig_sha256,
+    SHA256_DIGEST_LENGTH
+);
 
-    fn finalise(&mut self) -> [u8; SHA256_SIZE] {
-        let mut result = [0u8; SHA256_SIZE];
-        unsafe {
-            xdig_sha256_ctx_final(
-                self.ptr,
-                result.as_mut_ptr() as *mut std::os::raw::c_uchar,
-            );
-        };
-        return result;
-    }
-
-    fn digest(data: &[u8]) -> [u8; SHA256_SIZE] {
-        let mut result = [0u8; SHA256_SIZE];
-        unsafe {
-            xdig_sha256(
-                result.as_mut_ptr() as *mut std::os::raw::c_uchar,
-                data.as_ptr() as *const std::os::raw::c_void,
-                data.len(),
-            );
-        };
-        return result;
-    }
-}
